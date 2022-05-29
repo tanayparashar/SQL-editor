e562f66eed541781fb97dd181e884c79ddea32f5
Tanay Parashar <tanay@patch.localdomain>
2022-05-30 02:58:33 +0530

    editor and output done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
 import {Editor} from "./components/editor"
+//import {CsvToTable} from "./components/csvToTable"
+import { useEffect,useState } from 'react';
+import categories from "./assets/employees.csv"
 function App() {
+  const [ text, setText ] = useState();
+  const [csvArray, setCsvArray] = useState([]);
+
+const processCSV = (str, delim=',') => {
+        const headers = str.slice(0,str.indexOf('\n')).split(delim);
+        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+
+        const newArray = rows.map( row => {
+            const values = row.split(delim);
+            const eachObject = headers.reduce((obj, header, i) => {
+                obj[header] = values[i];
+                return obj;
+            }, {})
+            return eachObject;
+        })
+        console.log(newArray);
+        setCsvArray(newArray)
+    }
+  useEffect(()=>{
+    const file = categories;
+    fetch( file )
+            .then( response => response.text() )
+            .then( responseText => {
+                setText( responseText );
+                processCSV(responseText);
+            });
+  },[]);
   return (
     <div className="App">
       <div className='navbar'>
@@ -10,6 +40,27 @@ function App() {
         <Editor/>
       </div>
       <div>
+        {csvArray.length>0 ? 
+            <>
+                <table>
+                    <thead>
+                      {
+                        Object.keys(csvArray[0]).map(ele=><th>{ele}</th>)
+                      }
+
+                    </thead>
+                    <tbody>
+                      {csvArray.map((item) => (
+        <tr key={item.id}>
+          {Object.values(item).map((val) => (
+            <td>{val}</td>
+          ))}
+        </tr>
+      ))}
+                    </tbody>
+                </table>
+            </> : null}
+
       </div>
     </div>
   );
---
+++ src/components/csvToTable.js
@@ -0,0 +1,7 @@
+import employees from "./assets/employees.csv"
+import categories from "./assets/categories.csv"
+const CsvToTable=()=>{
+    return{
+        
+    };
+}

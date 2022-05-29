876e529f8585acf8d3eeea3dd9fdded8d9390efb
Tanay Parashar <tanay@patch.localdomain>
2022-05-29 23:31:53 +0530

    editor and output done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Editor} from "./components/editor"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <div style={{margin:"10px"}}><h3 style={{color:"#0acfa9"}}>SQL Editor</h3></div>
+      </div>
+      <div>
+        <Editor/>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
---
+++ src/components/editor.js
@@ -0,0 +1,63 @@
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import { useState } from 'react';
+import redo from "../assets/redo.png"
+export const Editor=()=>
+{
+  const [code, setCode] = useState(`SELECT * FROM Customers \nORDER BY Country ASC, CustomerName DESC;`);
+  const [output,setOutput]=useState("");
+  function callAPI(){
+      /*
+        Call API with ${code}
+        const data = { query: code};
+
+        fetch('https://example.com/query', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => setOutput(data))
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+
+      */
+     setOutput("null");
+     return "";
+  }
+  return (
+    <div>
+      <div className='editorTop'>
+        <div  style={{marginRight:"10px"}}>
+          <button className='clearButton'><img src={redo} alt="clear" onClick={()=>{setCode("")}} height={18} width={20}/></button>
+        </div>
+        <div>
+          <button className='runButton' onClick={callAPI}>
+            RUN
+          </button>
+        </div>
+      </div>
+      <CodeEditor
+        value={code}
+        language="sql"
+        placeholder="Please enter sql queries."
+        onChange={(evn) => {setCode(evn.target.value);console.log(evn.target.value)}}
+        padding={15}
+        minHeight={600}
+        style={{
+          fontSize: 18,
+          color:"white",
+          backgroundColor: "#242526",
+          fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          minHeight:"80%",
+        }}
+      />
+      <div>
+        <h3>Output:</h3>
+        <div>{output}</div>
+      </div>
+    </div>
+  );
+}
